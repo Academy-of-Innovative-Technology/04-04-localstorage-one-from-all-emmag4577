15bafd89c1afb8aef02d2a66a1809feb2d76a188
emma <egujral26@aoiths.org>
2026-03-15 20:20:00 -0400

    added JS

--- src/script/script.js
+++ src/script/script.js
@@ -4,7 +4,15 @@ var Database = {
 };
 
 function loadDataSource() {
+function loadDataSource() {
 
+  var storedData = localStorage.getItem(Database.keyName);
+
+  var parsedData = JSON.parse(storedData);
+
+  Database.data = parsedData.response;
+
+}
 
 }
 
@@ -13,29 +21,53 @@ function searchForMutantByAlias(mutantAlias) {
 
   var index = -1;
 
+  Database.data.forEach(function(mutant, i){
 
+    if(mutant.name.alias === mutantAlias){
+      index = i;
+    }
 
-
-
+  });
 
   if (index === -1) {
     alert("Invalid Alias");
   } else {
     displayData(index);
   }
+
 }
 
 
 function displayData(index) {
 
-  var htmlTemplate = ``;
+  var mutant = Database.data[index];
 
+  var htmlTemplate = `
+  
+  <div class="card p-4">
+    <div class="row">
 
+      <div class="col-md-4">
+        <img src="${mutant.image}" class="img-fluid">
+      </div>
 
+      <div class="col-md-8">
+        <h2>${mutant.name.alias}</h2>
+        <h5>${mutant.name.firstName} ${mutant.name.lastName}</h5>
 
+        <p><strong>Gender:</strong> ${mutant.profile.gender}</p>
+        <p><strong>Eyes:</strong> ${mutant.profile.eyes}</p>
+        <p><strong>Hair:</strong> ${mutant.profile.hair}</p>
+        <p><strong>Height:</strong> ${mutant.profile.height}</p>
 
+        <p><strong>Powers:</strong> ${mutant.powers.join(", ")}</p>
+        <p><strong>Affiliations:</strong> ${mutant.affiliation.join(", ")}</p>
+      </div>
 
+    </div>
+  </div>
 
+  `;
 
   document.querySelector("#results-section").innerHTML = htmlTemplate;
 }
